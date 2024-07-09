386fcb55a62ab0ef5602dea0687b6ab3db9be894
= <muthuannamalai@example.com>
2024-07-09 23:58:00 +0100

    linq build

---
+++ js/main.js
@@ -0,0 +1,150 @@
+const linqId = localStorage.getItem('linq');
+
+async function count() {
+    let url = 'http://localhost/linq-backend/';
+
+    try {
+        let res = await fetch(url);
+        let req = await res.text()
+        document.getElementById('thecount').innerHTML = req;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+count()
+
+async function fetchSaveLink() {
+    let url =  `http://localhost/linq-backend/save.php?linq=${linqId}`;
+    try {
+        let req = await fetch(url);
+        if(req.status === 200) {
+            let res = await req.text();
+            document.getElementById('thesavelink').innerHTML = res;
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+fetchSaveLink()
+function showside() {
+    let saveLink = document.getElementById('saveid');
+    if (saveLink.style.display === "none") {
+        saveLink.style.display = "block"
+    } else {
+        saveLink.style.display = "none"
+    }
+}
+
+function checkforid() {
+    let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+
+    //check for the id
+
+    let linQid = localStorage.getItem('linq');
+    if (linQid == null) {
+        for (let i = 0; i < characters.length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length))
+            localStorage.setItem('linq', result)
+        }
+
+    } else {
+        //create lingid
+        console.log('LinqId: ' + linQid)
+    }
+
+    //check the page url
+
+    let headLinks = window.location.href
+    let validatelink = headLinks.split('?');
+    let theParam = validatelink[1];
+
+    //init to send to server
+
+    async function sendLink() {
+        let url = `http://localhost/linq-backend/redirect.php?link=${theParam}`;
+        try {
+            let request = await fetch(url);
+            if (request.status == 200) {
+                let response = await request.text();
+                if (response !== 'null') {
+                    window.location.href = response;
+                }
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    sendLink()
+}
+
+function linqBuild() {
+    let linkName = document.getElementById("Name").value;
+    let defaultLink = document.getElementById('defaultURL').value
+
+    //validate the form
+
+    let err = document.getElementById('error');
+
+
+    try {
+        if (linkName == '' && defaultLink == '') { throw 'an error has occur, try checking the input' }
+        let pattern = /\s/;
+        if (linkName.match(pattern)) { throw "don't include space in the new link" }
+
+        //init the function that send's the data to the endpoint
+        document.getElementById('turn').style.display = 'block'
+        function sendDataToBe() {
+            async function sendRes() {
+                let url = `http://localhost/linq-backend/process.php?sessionid=${linqId}&default=${defaultLink}&linkName=${linkName}`;
+                let option = {
+                    method: 'GET'
+                }
+                try {
+                    let res = await fetch(url, option)
+                    if (res.status == 200) {
+                        let req = await res.text()
+                        let build = JSON.parse(req)
+                        err.innerHTML = build.link
+                        err.classList.add('done')
+                        document.getElementById('turn').style.display = 'none'
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+
+            sendRes()
+        }
+
+        sendDataToBe()
+    } catch (error) {
+        err.innerHTML = error
+        console.log(error)
+    }
+}
+
+function copy() {
+    let word = document.getElementById('error').innerHTML;
+    console.log(word)
+    navigator.clipboard.writeText(word);
+    console.log('text has been writing')
+    document.getElementById('clip').innerHTML = 'copied ';
+}
+
+async function updates() {
+    let url = 'http://localhost/linq-backend/update.php';
+
+    try {
+        let res = await fetch(url);
+        let req = await res.text()
+        document.getElementById('theupdate').innerHTML = req;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+updates()
